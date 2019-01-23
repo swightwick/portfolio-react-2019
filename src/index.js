961a1fecee330b1a9a4f3aef5991ea0de4ded3ed
Sam Wightwick <swightwick@gravitylondon.com>
2019-01-23 16:29:10 +0000

    updating year & copy changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ import {
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-132875181-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 
 ReactDOM.render(  <Router><Portfolio/></Router>, document.getElementById('root'));
 registerServiceWorker();
+
+
+
